b14fed8a2b67c6613da4a8cf7e68ffde9e4c5d41
TerezaR2D2 <vrtiskova.tereza12@gmail.com>
2026-02-06 10:39:06 +0100

    6.2.26

---
+++ 2. lekce/script.js
@@ -0,0 +1,12 @@
+alert("zkouška")
+alert(3)
+
+let userName = "adelka";
+console.log(userName);
+
+userName = "Honzik"
+console.log(userName);
+
+console.log(7+4);
+console.log("7" + "4");
+console.log("7" + 4);
