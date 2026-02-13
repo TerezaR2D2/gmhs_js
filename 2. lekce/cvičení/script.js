74aa669a92a5a5c1fa47ed79ee0cdf2f96bf1bc9
TerezaR2D2 <vrtiskova.tereza12@gmail.com>
2026-02-13 10:38:54 +0100

    13.02.26

---
+++ 2. lekce/cvičení/script.js
@@ -0,0 +1,14 @@
+alert("zkouška");
+alert(3);
+
+// proměnné pro jména 
+
+let userName = "Tereza"; 
+console.log(userName);
+
+userName = "violka";
+console.log(userName);
+
+// proměnné pro věk obou lidí
+
+
