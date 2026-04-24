388f3dccbf48654d096c64f03a5a7297d5c7681e
TerezaR2D2 <vrtiskova.tereza12@gmail.com>
2026-04-24 10:28:22 +0200

    24.04.26

--- 8.lekce/app.js
+++ 8.lekce/app.js
@@ -15,7 +15,7 @@ function showGreeting() {
 
 showGreeting();
 
-function funkceUvnitr (); {
+function funkceUvnitr() {
     let cislo = 123;
     console.log(cislo);
 }
@@ -32,9 +32,12 @@ test("Tomáš");
 test("Ema");
 test(123)
 
-function scitani(num1, num2) {
+function scitani(num1, num2) 
     console.log(num1 + num2)
-}
+    console.log(num1 - num2)
+    console.log(num1 * num2)
+    console.log(num1 / num2)
+
 
 scitani(4,6)
 
@@ -55,3 +58,25 @@ function deleni(num1, num2) {
 }
 
 deleni(24,6)
+
+
+//napiš funkci printRange, která vypíše do
+//konzole všechna čísla od parametru start až po end
+//postup:
+//1) deklarace funkce
+//2)definování parametrů
+//3)for smyčka
+//4)vyvolání funkce a argumentů
+
+function printRange(start, end) {
+    for (let i = start; i <= end; i++){
+        console.log(i);
+        }
+}
+
+    printRange(1, 10);
+    
+
+
+
+
