923a0c367f841b8aedb7d88c846de0e977cbaa8f
TerezaR2D2 <vrtiskova.tereza12@gmail.com>
2026-05-22 10:35:44 +0200

    22.05.26

---
+++ 10 lekce/script.js
@@ -0,0 +1,12 @@
+// Deklarace funkce
+// Cyklus for let i = (parametr); i <= (parametr)
+// console.log(i);
+
+// Volání funkce
+
+function printRange(start, end) {
+    for (let i = start; i <= end; i++){
+    console.log(i);
+    }
+}
+printRange(1,4)
