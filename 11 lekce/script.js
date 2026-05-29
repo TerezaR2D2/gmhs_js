06ae4954e78b8830a150842860f804e521a49c30
TerezaR2D2 <vrtiskova.tereza12@gmail.com>
2026-05-29 10:38:15 +0200

    29.5.26

---
+++ 11 lekce/script.js
@@ -0,0 +1,28 @@
+function multiply(a,b) {
+    let result = a * b;
+    return result;
+}
+
+console.log(multiply(5,10))
+
+function test() {
+    return "Hotovo";
+    console.log("tady by byl nějaký text")
+}
+
+console.log(test());
+
+function vek(age) {
+    if(age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(vek(18))
+
+console.log(9 % 2)
+
+// Deklarace Fce
+//Vrací true pokud je sudé, vrací false pokud je liché
